9121a77dc47ae59766c8e1deb3e2b64434a120ba
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-04-24 23:55:46 -0600

    Add basic javascript homework

---
+++ WebDev/H5/js/script.js
@@ -0,0 +1,102 @@
+import { findUnique, bubblesort, invertNew, invert, capitalize, gcd, hackerSpeak, factorize, removeDuplicates, 
+         smallestStringSize, isPalindrome, sortStrings, getStats, trendyString, isPow2, sortDescending } 
+    from "./utils.js";
+
+
+// If you want to test the functions directly in your browser's console uncomment the following (but this might overwrite existing functions)
+
+/* let funcs = [findUnique, bubblesort, invertNew, invert, capitalize, gcd, hackerSpeak, factorize, removeDuplicates, 
+             smallestStringSize, isPalindrome, sortStrings, getStats, trendyString, isPow2, sortDescending]
+
+let totalFuncs = funcs.length;
+
+for (let i = 0; i < totalFuncs; i++) {
+    window[funcs[i].name] = funcs[i];
+} */
+
+// TEST FUNCTIONS
+console.log("1. First unique character in a string");
+console.log("Testing with 'abacddbec'");
+console.log(findUnique('abacddbec'));
+
+console.log("2. Bubble Sort");
+let arr = Array.from({length: 9}, () => Math.floor(Math.random() * 10));
+console.log(`Testing with [${arr}]`);
+bubblesort(arr);
+console.log(arr);
+
+console.log("3. Invert the elements of an array");
+console.log(`Testing with [${arr}]`);
+console.log(`3.1. Inverting in a new array:`)
+console.log(invertNew(arr));
+console.log(`3.2 Inverting in place`);
+invert(arr);
+console.log(arr);
+
+console.log("4. Capitalize the words in a string");
+console.log("Testing with 'hello world!'")
+console.log(capitalize('hello world!'));
+
+console.log("5. Calculate the GCD of 2 numbers");
+console.log("Testing with (105, 231)");
+console.log(gcd(105, 231));
+
+console.log("6. Translate to Hacker Speak");
+console.log("Testing with 'Javascript es divertido'");
+console.log(hackerSpeak("Javascript es divertido"));
+
+console.log("7. Factorize");
+console.log("Testing with 12");
+console.log(factorize(12));
+
+console.log("8. Remove duplicates from array");
+console.log("Testing with [1, 0, 1, 1, 0, 0]");
+console.log(removeDuplicates([1, 0, 1, 1, 0, 0]));
+
+console.log("9. Smallest string size in an array")
+console.log("Testing with ['Hello', 'world', 'from', 'TC2005B', 'and', 'Javascript']");
+console.log(smallestStringSize(['Hello', 'world', 'from', 'TC2005B', 'and', 'Javascript']));
+
+console.log("10. Check if a string is a palindrome");
+console.log("Testing with 'tacocat'");
+console.log(isPalindrome('tacocat'));
+
+console.log("11. Sort array of strings (alphabetically)");
+console.log("Testing with [\"Oh,\", \"hello\", \"people\", \"from\", \"the\", \"Internet\"]");
+console.log(sortStrings(["Oh,","hello", "people", "from", "the", "Internet"]));
+
+console.log("12. Find the median and the mode from a list of numbers");
+console.log("Testing with [1, 8, 2, 3, 4, 5, 2, 3, 2]");
+console.log(getStats([1, 8, 2, 3, 4, 5, 2, 3, 2]));
+
+
+console.log("13. Check the most frequent string in a list");
+console.log("Testing with the 4th, 5th and 6th stanzas from Numb by Elderbrook");
+arr = ["Oh, reachin' in the dark",
+        "Nothing's there to touch",
+        "I wish I could love", // You found the hidden cat of the main script: 🐱🧶
+        "But I'm still numb",
+        "But I'm still numb",
+        "I'm still numb",
+        "And the feeling's gone",
+        "But I'm still numb",
+        "Love, I can't be the only one"]
+console.log(trendyString(arr));
+
+
+console.log("14. Check if a number is a power of 2");
+console.log("Testing with 1/4");
+console.log(isPow2(1/4));
+
+
+ /* 🤓 Note:
+
+ For integers this function can be programmed to be extremly efficient (constant time complexity) in a low level language,
+since the binary representation of a power of 2 has a single bit on.
+
+In JavaScript however, we are doomed to be less efficient. So I took this as an advantage to create a more general function that supports floating point numbers
+*/
+console.log("15. Sort in descending order");
+arr = Array.from({length: 9}, () => Math.floor(Math.random() * 10));
+console.log(`Testing with [${arr}]`);
+console.log(sortDescending(arr));
---
+++ WebDev/H5/js/utils.js
@@ -0,0 +1,285 @@
+/**
+ * Takes a string and returns the first unique character or an empty string if no unique characters exist
+ * @param {string} str - A string of characters
+ * @return {string} The first unique character from the string
+ */
+export function findUnique(str) {
+    const found = {};
+    
+    // Populate object with key value pairs of each character and the number of times it appears.
+    for (let chr of str)
+        found[chr] = (found[chr] ?? 0) + 1;
+
+
+    for (let chr of str) {
+        if (found[chr] == 1)
+            return chr;
+    }
+
+    return "";
+}
+
+
+/**
+ * Sort an array of numbers in place using bubble sort
+ * @param {number[]} arr - An array of numbers
+ * @return {number[]} - The sorted array
+ */
+export function bubblesort(arr){
+    const size = arr.length;
+
+    for (let i = 1; i < size; i++) {
+        let swaps = 0
+        for (let j = 0; j < size - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                swaps += 1;
+            }
+        }
+        
+        // Check number of swaps to terminate early (if no swaps were made you are done)
+        if (swaps == 0)
+            return arr
+    }
+
+    return arr;
+}
+
+/**
+ * Given an array of numbers, crete a new array that has all elements inverted
+ * @param {number[]} arr - An array of numbers
+ * @return {number[]} A new array with the elements inverted
+ */
+export function invertNew(arr) {
+    const newArr = new Array();
+
+    const size = arr.length;
+
+    for (let i = 0; i < size; i++)
+        newArr[i] = arr[size - 1 -i];
+
+    return newArr;
+}
+
+/**
+ * Given an array of numbers,invert all elements in place
+ * @param {number[]} arr - An array of numbers
+ * @return {number[]} The same array, with the elements in reverse order
+ */
+export function invert(arr) {
+    const size = arr.length;
+    const bound = Math.floor(size / 2);
+
+    for (let i = 0; i < bound; i++) {
+        let temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+
+    return arr;
+}
+
+
+/**
+ * Capitalize the words in a string
+ * @param {string} str - A string of text
+ * @return {string} A new string with all the words capitalized
+ */
+export function capitalize(str) {
+    return str.split(" ").map(
+            (word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ")
+}
+
+
+/**
+ * Calculate the greatest common divisor of 2 numbers
+ * @param {number} a - A number
+ * @param {number} b - Another number
+ * @return {number} The gcd of a and b
+ */
+export function gcd(a, b) {
+    if (a == 0)
+        return b;
+    else if (b == 0)
+        return a;
+    else
+        return gcd(Math.min(a,b), Math.max(a,b) % Math.min(a, b));
+}
+
+/**
+ * Transform a string of text into Hacker / Leet Speak
+ * @param {string} str - A string of text
+ * @return {string} A new string of text in "Hacker Speak"
+ */
+export function hackerSpeak(str) {
+    const translate = {
+        "o": 0,
+        "i": 1,
+        "z": 2,
+        "e": 3,
+        "a": 4,
+        "s": 5,
+        "b": 6,
+        "t": 7,
+        "g": 9
+    }
+
+    return str.split("").map(chr => translate[chr.toLowerCase()] || chr).join("");
+}
+
+/**
+ * Get an array of all factors of a given number
+ * @param {number} num - A number
+ * @return {number[]} An array containing all the factors of the given number
+ */
+export function factorize(num) {
+    const factors = new Array();
+    for (let i = 1; i < num; i++) {
+        if (num % i == 0)
+            factors.push(i);
+    }
+
+    return factors;
+}
+
+
+/**
+ * Create an array of unique elements
+ * @param {any[]} arr - An array of elements
+ * @return {any[]} An array containing the elements of arr without repetition
+ */
+export function removeDuplicates(arr) {
+    const s = new Set(arr)
+    return Array.from(s);
+}
+
+/**
+ * Find the smallest string in an array
+ * @param {string[]} arr - An array of strings
+ * @return {number} The size of the smallest string in the array
+ */
+export function smallestStringSize(arr) {
+    let smallest = arr[0].length;
+    for (let str of arr)
+        smallest = str.length < smallest ? str.length : smallest;
+    
+    return smallest
+}
+
+/**
+ * Check if a string is a palindorme
+ * @param {string} str - A string of text
+ * @return {boolean} True if the string is a palindrome and false otherwise
+ */
+export function isPalindrome(str) {
+    const size = str.length;
+    const bound = Math.floor(size / 2);
+
+    for (let i = 0; i < bound; i++) {
+        if (str[i] != str[size - 1 -i])
+            return false;
+    }
+
+    return true;
+}
+
+/**
+ * Order an array of strings alphabetically (using a locale)
+ * @param {string[]} arr - A list of strings of text
+ * @return {string[]} The array of strings sorted alphabetically
+ */
+export function sortStrings(arr) {
+    return arr.sort((a, b) => a.localeCompare(b));
+}
+
+/**
+ * Find the meadian and the mode in a list (array) of numbers
+ * @param {number[]} nums - An array of nubers
+ * @return {Stats} An object containing the median and the mode
+ */
+export function getStats(nums) {
+    /**
+     * @typedef {object} Stats
+     * @property {number} median - The median of a set of data
+     * @property {number} mode - The mode of a set of data
+     */
+    const stats = {
+        median: 0,
+        mode: 0
+    };
+
+    const popularity = {};
+
+    /* I remember there's a way to find the median in linear time...
+       I'm to lazy 😴 to deal with that tho. So I will sort and make it run in O(n lg n) time :3
+       In this case the side effect of sorting might be undesirable, so I will create a sorted copy of the array. */
+
+    let sortedNums = [...nums].sort((a, b) => a - b);
+
+    let size = nums.length;
+    let midpoint = size / 2;
+    stats.median = size % 2 == 0 ? sortedNums[midpoint] + sortedNums[midpoint + 1] : sortedNums[Math.floor(midpoint)];
+
+    for (let num of nums)
+        popularity[num] = (popularity[num] ?? 0) + 1;
+
+    let max = 0
+    for (let item in popularity) {
+        if (popularity[item] > max) {
+            max = popularity[item];
+            stats["mode"] = Number(item);
+        }
+    }
+
+    return stats;
+}
+
+/**
+ * Find the most frequent string in an array of strings
+ * @param {string[]} arr - An array of strings
+ * @return {string} The most popular string
+ */
+export function trendyString(arr) {
+    // This is a vile copy of my algorithm to find the mode in a set of data...
+    let mode = "";
+
+    const popularity = {};
+
+    for (let str of arr)
+        popularity[str] = (popularity[str] ?? 0) + 1;
+
+    let max = 0
+    for (let item in popularity) {
+        if (popularity[item] > max) {
+            max = popularity[item];
+            mode = item;
+        }
+    }
+    
+    return mode;
+}
+
+
+/**
+ * Check if a number is a power of 2
+ * @param {number} num - A number
+ * @return {boolean} True if the number is a power of 2 and false otherwise
+ */
+export function isPow2(num) {
+    return Number.isInteger(Math.log2(num));
+}
+
+/**
+ * Sort numbers in descending order (in place)
+ * @param {number[]} nums - An array of numbers
+ * @return {number[]} The same array (but sorted)
+ */
+export function sortDescending(nums) {
+    return nums.sort((a, b) => b - a);
+}
+
+// You made it to the end!
+// You found the hidden cat: 🐱
