a03ba0f621fc2fb199c15325655492581cc1eab9
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-05-01 16:22:59 -0600

    Reduce index to avoid overflow

--- WebDev/H5/js/utils.js
+++ WebDev/H5/js/utils.js
@@ -27,10 +27,10 @@ export function findUnique(str) {
 export function bubblesort(arr){
     const size = arr.length;
 
-    for (let i = 0; i < size; i++) {
+    for (let i = 1; i < size; i++) {
         let swaps = 0
         for (let j = 0; j < size - i; j++) {
-            if(arr[j] >= arr[j + 1]) {
+            if(arr[j] > arr[j + 1]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
